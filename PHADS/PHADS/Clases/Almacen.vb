acacc780db8489a057a9482cb5b6c902dc349f32
351-18\admin-dam2 <mikelu11697@gmail.com>
2018-06-11 21:08:38 +0200

    Clase tienda y clase almacén añadidas

---
+++ PHADS/PHADS/Clases/Almacen.vb
@@ -0,0 +1,93 @@
+﻿Public Class Almacen
+#Region "Variables"
+    Private _Lote As String
+    Private _Id_Producto As Integer
+    Private _Id_Tienda As Integer
+    Private _Cantidad As Integer
+    Private _Fecha_Cad As DateTime
+#End Region
+
+#Region "Propiedades"
+    Public Property Lote As String
+        Get
+            Return _Lote
+        End Get
+        Set(value As String)
+            _Lote = value
+        End Set
+    End Property
+
+    Public Property Id_Producto As Integer
+        Get
+            Return _Id_Producto
+        End Get
+        Set(value As Integer)
+            _Id_Producto = value
+        End Set
+    End Property
+
+    Public Property Id_Tienda As Integer
+        Get
+            Return _Id_Tienda
+        End Get
+        Set(value As Integer)
+            _Id_Tienda = value
+        End Set
+    End Property
+
+    Public Property Cantidad As Integer
+        Get
+            Return _Cantidad
+        End Get
+        Set(value As Integer)
+            _Cantidad = value
+        End Set
+    End Property
+
+    Public Property Fecha_Cad As Date
+        Get
+            Return _Fecha_Cad
+        End Get
+        Set(value As Date)
+            _Fecha_Cad = value
+        End Set
+    End Property
+#End Region
+
+#Region "Constructores"
+    Public Sub New(Row As DataRow)
+        Me.Lote = Row(0)
+        Me.Id_Producto = Row(1)
+        Me.Id_Tienda = Row(2)
+        Me.Cantidad = Row(3)
+        Me.Fecha_Cad = Row(4)
+    End Sub
+#End Region
+
+#Region "metodos"
+    Public Function SqlInsert() As String
+        Dim Query As String = "INSERT INTO Almacen(Lote,_Id_Producto,_Id_Tienda,_Cantidad,_Fecha_Cad) VALUES('"
+        Query &= Me.Lote & "',"
+        Query &= Me.Id_Producto & ",'"
+        Query &= Me.Id_Tienda & "','"
+        Query &= Me.Cantidad & "','"
+        Query &= "str_to_date('" & Me.Fecha_Cad & "','%d/%m/%y'))"
+        Return Query
+    End Function
+
+    Public Function SqlUpdate() As String
+        Dim Query As String = "UPDATE Tienda SET"
+        Query &= "Lote = '" & Me.Lote & "', "
+        Query &= "Id_Producto = " & Me.Id_Producto & ", "
+        Query &= "Id_Tienda ='" & Me.Id_Tienda & "',"
+        Query &= "Cantidad ='" & Me.Cantidad & "',"
+        Query &= "str_to_date('" & Me.Fecha_Cad & "','%d/%m/%y'))"
+        Return Query
+    End Function
+
+    Public Function SqlDelete() As String
+        Dim Query As String = "DELETE FROM Almacen WHERE Lote=" & Me.Lote & "and Id_Producto=" & Me.Id_Producto
+        Return Query
+    End Function
+#End Region
+End Class
--- PHADS/PHADS/Clases/Tienda.vb
+++ PHADS/PHADS/Clases/Tienda.vb
@@ -1,31 +1,37 @@
-﻿Public Class Proveedor
+﻿Public Class Tienda
 #Region "Variables"
-    Private _Id_Proveedor As Integer
-    Private _Nombre As String
+    Private _Id_Tiendas As Integer
+    Private _Id_Gerente As String
     Private _Tipo_Via As Integer
     Private _Nombre_Via As String
     Private _No_Via As String
+    Private _No_Ptal As String
+    Private _Piso As String
+    Private _Puerta As String
     Private _Cod_Postal As String
     Private _Provincia As String
     Private _Localidad As String
 #End Region
+
 #Region "Propiedades"
-    Public Property Id_Proveedor As Integer
+    Public Property Id_Tiendas As Integer
         Get
-            Return _Id_Proveedor
+            Return _Id_Tiendas
         End Get
         Set(value As Integer)
-            _Id_Proveedor = value
+            _Id_Tiendas = value
         End Set
     End Property
-    Public Property Nombre As String
+
+    Public Property Id_Gerente As String
         Get
-            Return _Nombre
+            Return _Id_Gerente
         End Get
         Set(value As String)
-            _Nombre = value
+            _Id_Gerente = value
         End Set
     End Property
+
     Public Property Tipo_Via As Integer
         Get
             Return _Tipo_Via
@@ -34,6 +40,7 @@
             _Tipo_Via = value
         End Set
     End Property
+
     Public Property Nombre_Via As String
         Get
             Return _Nombre_Via
@@ -42,6 +49,7 @@
             _Nombre_Via = value
         End Set
     End Property
+
     Public Property No_Via As String
         Get
             Return _No_Via
@@ -50,6 +58,34 @@
             _No_Via = value
         End Set
     End Property
+
+    Public Property No_Ptal As String
+        Get
+            Return _No_Ptal
+        End Get
+        Set(value As String)
+            _No_Ptal = value
+        End Set
+    End Property
+
+    Public Property Piso As String
+        Get
+            Return _Piso
+        End Get
+        Set(value As String)
+            _Piso = value
+        End Set
+    End Property
+
+    Public Property Puerta As String
+        Get
+            Return _Puerta
+        End Get
+        Set(value As String)
+            _Puerta = value
+        End Set
+    End Property
+
     Public Property Cod_Postal As String
         Get
             Return _Cod_Postal
@@ -58,6 +94,7 @@
             _Cod_Postal = value
         End Set
     End Property
+
     Public Property Provincia As String
         Get
             Return _Provincia
@@ -66,6 +103,7 @@
             _Provincia = value
         End Set
     End Property
+
     Public Property Localidad As String
         Get
             Return _Localidad
@@ -75,48 +113,57 @@
         End Set
     End Property
 #End Region
+
 #Region "Constructores"
     Public Sub New(Row As DataRow)
-        Me.Id_Proveedor = Row(0)
-        Me.Nombre = Row(1)
+        Me.Id_Tiendas = Row(0)
+        Me.Id_Gerente = Row(1)
         Me.Tipo_Via = Row(2)
         Me.Nombre_Via = Row(3)
         Me.No_Via = Row(4)
-        Me.Cod_Postal = Row(5)
-        Me.Provincia = Row(6)
-        Me.Localidad = Row(7)
+        Me.No_Ptal = Row(5)
+        Me.Piso = Row(6)
+        Me.Puerta = Row(7)
+        Me.Cod_Postal = Row(8)
+        Me.Provincia = Row(9)
+        Me.Localidad = Row(10)
     End Sub
-
 #End Region
-#Region "Metodos"
+
+#Region "metodos"
     Public Function SqlInsert() As String
-        Dim Query As String = "INSERT INTO Proveedores(Nombre,Tipo_Via,Nombre_Via,No_Via,Cod_Postal,Provincia,Localidad) VALUES('"
-        Query &= Me.Nombre & "',"
+        Dim Query As String = "INSERT INTO Tienda(Id_Gerente,Tipo_Via,Nombre_Via,No_Via,Piso,Puerta,Cod_Postal,Provincia,Localidad) VALUES('"
+        Query &= Me.Id_Gerente & "',"
         Query &= Me.Tipo_Via & ",'"
         Query &= Me.Nombre_Via & "','"
         Query &= Me.No_Via & "','"
+        Query &= Me.No_Ptal & "','"
+        Query &= Me.Piso & "','"
+        Query &= Me.Puerta & "','"
         Query &= Me.Cod_Postal & "','"
         Query &= Me.Provincia & "','"
         Query &= Me.Localidad & "')"
         Return Query
     End Function
+
     Public Function SqlUpdate() As String
-        Dim Query As String = "UPDATE Proveedores SET"
-        Query &= "Nombre = '" & Me.Nombre & "', "
+        Dim Query As String = "UPDATE Tienda SET"
+        Query &= "Id_Gerente = '" & Me.Id_Gerente & "', "
         Query &= "Tipo_Via = " & Me.Tipo_Via & ", "
         Query &= "Nombre_Via ='" & Me.Nombre_Via & "',"
         Query &= "No_Via ='" & Me.No_Via & "',"
+        Query &= "No_Ptal ='" & Me.No_Ptal & "',"
+        Query &= "Piso ='" & Me.Piso & "',"
+        Query &= "Puerta ='" & Me.Puerta & "',"
         Query &= "Cod_Postal ='" & Me.Cod_Postal & "',"
         Query &= "Provincia = '" & Me.Provincia & "',"
         Query &= "Localidad = '" & Me.Localidad & "'"
-        Query &= "WHERE Id_Proveedore =" & Me.Id_Proveedor
-        Return Query
+        Query &= "WHERE Id_Tiendas =" & Me.Id_Tiendas
     End Function
+
     Public Function SqlDelete() As String
-        Dim Query As String = "DELETE FROM Proveedores WHERE Id_Proveedore=" & Me.Id_Proveedor
+        Dim Query As String = "DELETE FROM Tienda WHERE Id_tiendas=" & Me.Id_Tiendas
         Return Query
     End Function
 #End Region
-
-
 End Class
